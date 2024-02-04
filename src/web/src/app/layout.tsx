85aac0d7a00f39722b5110bd6fbe84b60bf2c6fd
Yumin Cho <dbals081216@gmail.com>
2024-02-04 23:19:52 +0900

    :heavy_minus_sign: Remove tailwind

--- src/web/src/app/layout.tsx
+++ src/web/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
