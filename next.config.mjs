1aa13082aab2c498726e548df326bc7cc0086411
yuwol <yuwol036@gmail.com>
2024-05-01 20:23:32 +0900

    :sparkles: Add environment variable type validator

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import createJITI from 'jiti'
+import { fileURLToPath } from 'url'
+
+const jiti = createJITI(fileURLToPath(import.meta.url))
+jiti('./src/env')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
