cdbb5a92372bb580c7f2d2c4177afdb420553cdd
yuwol <yuwol036@gmail.com>
2024-05-05 19:46:22 +0900

    :lipstick: Style submit button

--- src/ui/atoms/SubmitButton.tsx
+++ src/ui/atoms/SubmitButton.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import clsx from 'clsx'
 import { useFormStatus } from 'react-dom'
 
-export const SubmitButton: React.FC<{ text: string }> = ({ text }) => {
+import styles from './SubmitButton.module.css'
+
+export const SubmitButton: React.FC<{
+  text: string
+  primary?: boolean
+  disabled?: boolean
+}> = ({ text, primary, disabled }) => {
   const { pending } = useFormStatus()
 
   return (
-    <button type="submit" disabled={pending} aria-disabled={pending}>
+    <button
+      type="submit"
+      disabled={disabled || pending}
+      aria-disabled={disabled || pending}
+      className={clsx(styles.button, {
+        [styles.primary]: primary,
+      })}
+    >
       {text}
     </button>
   )
